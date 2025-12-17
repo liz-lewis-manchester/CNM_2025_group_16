df73eb17ae6f6731efc9dacea4fef55211d8f30e
zifanpeng040-sys <zifanpeng040@gmail.com>
2025-12-17 03:20:31 +0000

    plotting_simple.py

--- src/plotting_simple.py
+++ src/plotting_simple.py
@@ -5,19 +5,19 @@ def plot_heatmap_xt(x, t, C, save_path=None):
   #plot C(t,x) heatmap
   #x is nx, t is nt, C is matrix of nt,nx
   #save_path is used for graph saving
-  x = np.array(x)
-  t = np.array(t)
-  C = np.array(C)
+  x = np.array(x)    #convert x to numpy
+  t = np.array(t)    #convert t to numpy
+  C = np.array(C)    #convert C to numpy
 
-  if C.shape ! = (len(t),len(x)):
-    raise ValueError("Shape of C should be (len(t), len(x)) or '(nt,nx)', try again")  #for external user
+  if C.shape != (len(t),len(x)):
+    raise ValueError("Shape of C should be (len(t), len(x)) or '(nt,nx)', try again")  #for external user to check and import correct C
 
   plt.figure(figsize=(8, 5))   #adjust size of graph
 
-  plt.imshow(
+  plt.imshow(           # creat heatmap
     C,
-    aspect="auto"
-    origin="lower"
+    aspect="auto",
+    origin="lower",
     extent=[x.min(), x.max(), t.min(), t.max()]
   )
 
@@ -32,3 +32,4 @@ def plot_heatmap_xt(x, t, C, save_path=None):
     plt.savefig(save_path,dpi=300)
 """
 plt.show()
+plot_heatmap_xt(x, t, C)
