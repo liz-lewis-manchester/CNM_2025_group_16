e364fac1d3416dfb441d94adfa1b6f3ce7260c9f
monyka696 <monyka.touch@student.manchester.ac.uk>
2025-12-17 14:08:03 +0000

    Updated the code
    Made parameters clearer and fixed keys errors.

--- datahandling.py
+++ datahandling.py
@@ -1,12 +1,17 @@
+import pandas as pd
+import numpy as np
 #extract the data from each column into an array and use the flatten function to convert data into 1-dimensional
 def read_data(length, spatial_resolution, filename):
     df = pd.read_csv(filename, encoding='latin1')#use encoding='latin1' because the symbol mu cannot be read
-    return df[['Concentration (µg/m_ )']].values.flatten()
+    conc_list = df[['Concentration (µg/m_ )']].values.flatten() # Corrected potential trailing space
+    distance_list = df[['Distance (m)']].values.flatten()     # Corrected extra space
+    return conc_list, distance_list
+#print(read_data(20, 0.2, 'initial_conditions.csv'))
     #return concentration list
 
 def interpolation(length, spatial_resolution, conc_list, distance_list):
-    distance_grid = np.arange(0, length + spatial_resolution, spatial_resolution) 
+    distance_grid = np.arange(0, length + spatial_resolution, spatial_resolution)
     conc_grid = np.interp(distance_grid, distance_list, conc_list)
     return conc_grid, distance_grid
-conc_raw, distance_raw = read_data(20, 0.2, 'initial_conditions.csv') #example values for conc_list, distance_list parameters
+#conc_raw, distance_raw = read_data(20, 0.2, 'initial_conditions.csv') #example values for conc_list, distance_list parameters
 #print(interpolation(20,0.2, conc_raw, distance_raw))
