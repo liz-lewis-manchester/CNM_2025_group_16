fed3a49b6158131b4716bc8b6415d09d78453e23
zifanpeng040-sys <zifanpeng040@gmail.com>
2025-12-17 02:12:45 +0000

    Update and rename ploting.py to initial_plotting.py

---
+++ src/initial_plotting.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import numpy as np
+df = pd.read_csv('initial_condition.csv', encoding = 'latin1')    #"latin1" was used to avoid special symbol
+
+S = df["Distance(m)"].values
+C = df["Concentration(µg/m_)"].values
+
+x = np.arrange(0, 20, 0.2)   #create grid
+C0 = np.interp(x, S, C)
+
+print("x shape", x.shape)
+print("C0 shape", C0.shape)
+
+plt.figure()
+plt.plot(x,C0)
+plt.xlabel("Distance x (m)")
+plt.ylabel("Concentration C (µg/m³)")
+plt.title("Initial pollutant concentration")
+plt.grid(True)            #use grid for easiler reading
+plt.show()
--- src/ploting.py
+++
@@ -1,3 +0,0 @@
-import pandas as pd
-import numpy as np
-df = pd.read_csv('initial_condition.csv', encoding = 'latin1')
